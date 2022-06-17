f43ecb9f6c0fecc6e66842e73dcdef386cab3517
Isaac <isaacfrett2@gmail.com>
2022-06-17 13:48:23 -0500

    Modfied Scraper.py to assing variables to scraped data allowing for connection with Database.py

--- Scraper.py
+++ Scraper.py
@@ -1,16 +1,14 @@
-# Scan popular job board websites like indeed, glassdoor, ziorecruiter, and linkedin with a specifc key term or phrase
+# Scan popular job board websites
 # Retrun all postings from that keyword/phrase into a table
-# Table will show the title, company, location, link, and the website its from
-# Table can be sorted by location, title, site, etc.
-# Table can check for and remove duplicate entries
-# Maybe feature can show if we applied yet or not
-
+# Table will show the title, company, location
+# Table can be sorted by location, title, job
 
 
 import requests
 from bs4 import BeautifulSoup
 from helium import *
 
+entries = []
 
 class Scraper:
 
@@ -29,15 +27,11 @@ class Scraper:
         job_list = results.find_all("td", class_="resultContent")
 
         for job in job_list:
-            title_element = job.find("a", class_="jcs-JobTitle")
-            company_element = job.find("span", class_="companyName")
-            location_element = job.find("div", class_="companyLocation")
-            print(title_element.text.strip())
-            print(company_element.text.strip())
-            print(location_element.text.strip())
-            print()
-
-
+            title = job.find("a", class_="jcs-JobTitle").text.strip()
+            company = job.find("span", class_="companyName").text.strip()
+            location = job.find("div", class_="companyLocation").text.strip()
+            entries.append([title, company, location])
+            
 
     def glassdoor():
 
@@ -52,15 +46,11 @@ class Scraper:
         job_list = results.find_all("div", class_="d-flex flex-column pl-sm css-1buaf54 job-search-key-1mn3dn8 e1rrn5ka0")
 
         for job in job_list:
-            title_element = job.find("a", class_="jobLink job-search-key-1rd3saf eigr9kq1")
-            company_element = job.find("a", class_="job-search-key-l2wjgv e1n63ojh0 jobLink")
-            location_element = job.find("span", class_="css-1buaf54 pr-xxsm job-search-key-iii9i8 e1rrn5ka4")
-            print(title_element.text.strip())
-            print(company_element.text.strip())
-            print(location_element.text.strip())
-            print()
-
-
+            title = job.find("a", class_="jobLink job-search-key-1rd3saf eigr9kq1").text.strip()
+            company = job.find("a", class_="job-search-key-l2wjgv e1n63ojh0 jobLink").text.strip()
+            location = job.find("span", class_="css-1buaf54 pr-xxsm job-search-key-iii9i8 e1rrn5ka4").text.strip()
+            entries.append([title, company, location])
+            
 
     def linkedin():
 
@@ -74,15 +64,11 @@ class Scraper:
 
         job_list = results.find_all("div", class_="base-search-card__info")
         for job in job_list:
-            title = job.find("h3", class_="base-search-card__title")
-            company = job.find("h4", class_="base-search-card__subtitle")
-            location = job.find("span", class_="job-search-card__location")
-            print(title.text.strip())
-            print(company.text.strip())
-            print(location.text.strip())
-            print()
-
-
+            title = job.find("h3", class_="base-search-card__title").text.strip()
+            company = job.find("h4", class_="base-search-card__subtitle").text.strip()
+            location = job.find("span", class_="job-search-card__location").text.strip()
+            entries.append([title, company, location])
+            
 
     def simply_hired():
 
@@ -97,15 +83,11 @@ class Scraper:
         job_list = results.find_all("div", class_="SerpJob-jobCard card")
         
         for job in job_list:
-            title = job.find("a", class_="SerpJob-link card-link")
-            company = job.find("span", class_="JobPosting-labelWithIcon jobposting-company")
-            location = job.find("span", class_="jobposting-location")
-            print(title.text.strip())
-            print(company.text.strip())
-            print(location.text.strip())
-            print()
-
-
+            title = job.find("a", class_="SerpJob-link card-link").text.strip()
+            company = job.find("span", class_="JobPosting-labelWithIcon jobposting-company").text.strip()
+            location = job.find("span", class_="jobposting-location").text.strip()
+            entries.append([title, company, location])
+        
 
     def gov_jobs():
 
@@ -120,19 +102,17 @@ class Scraper:
         job_list = results.find_all("li", class_="job-item")
         
         for job in job_list:
-            title = job.find("a", class_="job-details-link")
+            title = job.find("a", class_="job-details-link").text.strip()
             company = "Government"
-            location = job.find("div", class_="primaryInfo job-location")
-            print(title.text.strip())
-            print(company)
-            print(location.text.strip())
-            print()
-    
-    indeed()
-    glassdoor()
-    linkedin()
-    simply_hired()
-    gov_jobs()
+            location = job.find("div", class_="primaryInfo job-location").text.strip()
+            entries.append([title, company, location])
+            
 
-# Add regex to sort through data and find "entry" or "junior" positions
-# Add data to sql database for manipulation(ie: checkmark box to signify applied or not, sort by(location))
+def run_all():
+    Scraper.gov_jobs()
+    Scraper.indeed()
+    Scraper.glassdoor()
+    Scraper.linkedin()
+    Scraper.simply_hired()
+    
+run_all()
