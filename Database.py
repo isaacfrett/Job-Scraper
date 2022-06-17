fc6dd6a3f4107816ca7424be9f266e08fdb3ef07
Isaac <isaacfrett2@gmail.com>
2022-06-17 13:47:49 -0500

    Linked database to mysql to commit data into table. Now fully viewable.

--- Database.py
+++ Database.py
@@ -1,5 +1,7 @@
 import mysql.connector
+from Scraper import entries
 
+#This will establish our conenction to our SQL server and initalize our database
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -7,22 +9,25 @@ db = mysql.connector.connect(
     database="JobScraper"
 )
 
-mycursor = db.cursor()
+mycursor = db.cursor(buffered = True)
 
-mycursor.execute("CREATE TABLE Jobs (title VARCHAR(100), company VARCHAR(50), location VARCHAR(50), jobID int PRIMARY KEY AUTO_INCREMENT)")
+#This will create an SQL Table with 4 columns including and ID for our primary key
+#mycursor.execute("CREATE TABLE Jobs (title VARCHAR(100), company VARCHAR(50), location VARCHAR(50), jobID int PRIMARY KEY AUTO_INCREMENT)")
 
+#This function will bring in data from our Scraper.py file to convert it into our data table
 
-def AddEntries(self, jobtitle, company, location):
-    self = self
-    job = jobtitle
-    comp = company
-    loc = location
-    for entry in job_list:
-        mycursor.execute("INSERT INTO Jobs (title, company, location) VALUES ({job}, {comp}, {loc})")
+def addEntries():
+    for entry in entries:
+        title = entry[0]
+        company = entry[1]
+        location = entry[2]
+        if entry != mycursor.execute("SELECT * FROM Jobs"):
+            mycursor.execute("INSERT INTO Jobs (title, company, location) VALUES (%s,%s,%s)", (title, company, location))
+        else:
+            pass
+addEntries()
 
-
-#mycursor.commit()
-
-# mycursor.execute("SELECT # FROM Jobs")
-for x in mycursor:
-    print(x)
+db.commit()
+# mycursor.execute("SELECT * FROM Jobs WHERE location = 'Remote'")
+# for x in mycursor:
+#     print(x)
